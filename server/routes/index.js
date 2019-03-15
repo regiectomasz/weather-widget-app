f66e8192a41d366761d41717bdf90f62b846a5bf
Tomasz Regiec <regiectomasz@gmail.com>
2019-03-15 22:13:45 +0100

    feat(UI/API): connect UI and API (#1)
    - remove axios library (use fetch from js)
    - move mock from UI to API
    - API: add routes for mocks
    - UI: add update chart animation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,38 @@
-import { Router } from 'express';
+import {Router} from 'express';
+
+import {mockForecast, mockDaily} from '../mocks/weather.mock';
 
 const router = Router();
 
 /**
- * GET /api/info
+ * @swagger
+ *
+ * /api/info:
+ *    get:
+ *      description: get API info
  */
 router.get('/info', (req, res/*, next*/) => res.status(200).send({status: 'OK'}));
+
+/**
+ * @swagger
+ *
+ * /api/daily:
+ *    get:
+ *      description: get daily weather data
+ *      produces:
+ *       - "application/json"
+ */
+router.get('/daily', (req, res, next) => res.status(200).send(mockDaily));
+
+/**
+ * @swagger
+ *
+ * /api/forecast:
+ *    get:
+ *      description: get forecast weather data
+ *      produces:
+ *       - "application/json"
+ */
+router.get('/forecast', (req, res, next) => res.status(200).send(mockForecast));
 
 export default router;
--- src/components/weather-widget/weather-widget.jsx
+++ src/components/weather-widget/weather-widget.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import * as d3 from 'd3';
 
-import * as mock from './weather-widget.mock';
 import styles from './weather-widget.style.scss';
 
 export const daysAbbreviation = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -13,13 +12,47 @@ export const weatherIconHashMap = {
 };
 
 export default class WeatherWidget extends Component {
+	state = {
+		daily: {
+			list: []
+		},
+		forecast: {
+			list: []
+		},
+		currentDay: 0
+	};
+
+	componentDidMount() {
+		fetch('/api/daily')
+			.then(res => res.json())
+			.then(res => {
+				this.setState({daily: res});
+			});
+
+		fetch('/api/forecast')
+			.then(res => res.json())
+			.then(res => {
+				this.setState({forecast: res});
+			});
+	}
+
+	changeDay(currentDay) {
+		this.setState(() => {
+			return {currentDay};
+		});
+	}
+
 	render() {
-		const daysContent = mock.mockDaily.list.slice(0, 5).map(dayData => {
+		const daysContent = this.state.daily.list.slice(0, 5).map((dayData, index) => {
 			let date = new Date(dayData.dt * 1000); // convert Unix epoch time
 			let dayName = daysAbbreviation[date.getDay()];
 
+			let mainClassName = 'days-content';
+			if (this.state.currentDay === index)
+				mainClassName += ' active';
+
 			return (
-				<li className="days-content">
+				<li className={mainClassName} key={dayData.dt} onClick={() => this.changeDay(index)}>
 					<div className="font-weight-bold">{dayName}</div>
 					<span className={`icon ${weatherIconHashMap[dayData.weather[0].main]}`}/>
 					<div>
@@ -36,7 +69,8 @@ export default class WeatherWidget extends Component {
 				<ul className="content">
 					{daysContent}
 				</ul>
-				<BarChart width={styles.weatherWidgetWidth} height={160}/>
+				<BarChart width={styles.weatherWidgetWidth} height={160}
+						  data={this.state.forecast.list.slice(this.state.currentDay * 8, this.state.currentDay * 8 + 8)}/>
 			</div>
 		);
 	}
@@ -57,69 +91,94 @@ class BarChart extends React.Component {
 
 	updateChart() {
 		// TODO: performance update
-		let data = mock.mockForecast.list.slice(0, 8);
+		let data = this.props.data;
 		let svgWidth = this.props.width;
 		let svgHeight = this.props.height;
 
-		let minTemp = Math.round(Math.min(...data.map(d => d.main.temp_min)));
-		let maxTemp = Math.round(Math.max(...data.map(d => d.main.temp_max)));
+		console.log(this.props.data.map(d => d.main.temp));
 
-		let width = svgWidth / 8;
+		if (data && data.length) {
 
-		// 5 - min bar height
-		// 50 - free space for texts (label and value)
-		let yScale = d3.scaleLinear().domain([minTemp, maxTemp]).range([5, svgHeight - 50]); // or d3.scaleLinear(domain, range)
+			let minTemp = Math.round(Math.min(...data.map(d => d.main.temp_min)));
+			let maxTemp = Math.round(Math.max(...data.map(d => d.main.temp_max)));
 
-		const svg = d3.select(this.svgEl);
+			let width = svgWidth / 8;
 
-		window.test = yScale;
+			// 5 - min bar height
+			// 50 - free space for texts (label and value)
+			let yScale = d3.scaleLinear().domain([minTemp, maxTemp]).range([5, svgHeight - 50]); // or d3.scaleLinear(domain, range)
 
-		// bars
-		svg.select("g.bars")
-			.selectAll('rect')
-			.data(data)
-			.enter().append('rect')
-			.attr('class', 'bar')
-			// .attr('fill', styles.widgetGrayNormal) // moved to style
-			.attr('x', (datum, index) => index * width)
-			.attr('y', datum => svgHeight - Math.round(yScale(datum.main.temp)))
-			.attr('width', width)
-			.attr('height', datum => Math.round(yScale(datum.main.temp)));
+			const svg = d3.select(this.svgEl);
 
+			/**
+			 * bars
+			 */
+			let bars = svg.select('g.bars')
+				.selectAll('rect')
+				.data(data);
 
-		// values
-		svg.select("g.values")
-			.selectAll('text.value')
-			.data(data)
-			.enter().append('text')
-			.attr('class', 'value')
-			.text(datum => Math.round(datum.main.temp) + '\u00B0')
-			.attr('x', (datum, index) => index * width + width / 2)
-			.attr('y', datum => svgHeight - Math.round(yScale(datum.main.temp)) - 10);
+			// add
+			bars.enter().append('rect')
+				.attr('class', 'bar')
+				// .attr('fill', styles.widgetGrayNormal) // moved to style
+				.attr('x', (datum, index) => index * width)
+				.attr('y', datum => svgHeight - yScale(Math.round(datum.main.temp)))
+				.attr('width', width)
+				.attr('height', datum => yScale(Math.round(datum.main.temp)));
 
-		// labels
-		svg.select("g.labels")
-			.selectAll('text.label')
-			.data(data)
-			.enter().append('text')
-			.attr('class', 'label')
-			.text(datum => new Date(datum.dt * 1000).getHours() + ':00') // or split dt_txt
-			.attr('x', (datum, index) => index * width + width / 2)
-			.attr('y', 20);
+			// update
+			bars.transition()
+				.duration(500)
+				.attr('y', datum => svgHeight - yScale(Math.round(datum.main.temp)))
+				.attr('height', datum => yScale(Math.round(datum.main.temp)));
 
-		// grid (only vertical)
-		svg.select("g.grid")
-			.selectAll('line.vertical')
-			.data(new Array(data.length - 1))
-			.enter().append('line')
-			.attr('class', 'vertical')
-			.attr('x1', (datum, index) => ((index + 1)  * width))
-			.attr('y1', 0)
-			.attr('x2', (datum, index) => ((index + 1)  * width))
-			.attr('y2', svgHeight);
+			/**
+			 * values
+			 */
+			let values = svg.select('g.values')
+				.selectAll('text.value')
+				.data(data);
 
-		svg.exit().remove();
+			// add
+			values.enter().append('text')
+				.attr('class', 'value')
+				.text(datum => Math.round(datum.main.temp) + '\u00B0')
+				.attr('x', (datum, index) => index * width + width / 2)
+				.attr('y', datum => svgHeight - yScale(Math.round(datum.main.temp)) - 10);
 
+			// update
+			values
+				.text(datum => Math.round(datum.main.temp) + '\u00B0')
+				.transition()
+				.duration(500)
+				.attr('y', datum => svgHeight - yScale(Math.round(datum.main.temp)) - 10);
+
+			// labels
+			let labels = svg.select('g.labels')
+				.selectAll('text.label')
+				.data(data);
+
+			labels.enter().append('text') // add
+				.attr('class', 'label')
+				.text(datum => new Date(datum.dt * 1000).getHours() + ':00') // or split dt_txt
+				.attr('x', (datum, index) => index * width + width / 2)
+				.attr('y', 20);
+
+			labels.text(datum => new Date(datum.dt * 1000).getHours() + ':00'); // update
+
+			// grid (only vertical)
+			svg.select('g.grid')
+				.selectAll('line.vertical')
+				.data(new Array(data.length - 1))
+				.enter().append('line')
+				.attr('class', 'vertical')
+				.attr('x1', (datum, index) => ((index + 1) * width))
+				.attr('y1', 0)
+				.attr('x2', (datum, index) => ((index + 1) * width))
+				.attr('y2', svgHeight);
+
+			svg.exit().remove();
+		}
 	}
 
 	render() {
