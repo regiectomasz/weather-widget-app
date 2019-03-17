eafb676d8e71af6e8b450b2e65d877937c4c52aa
Tomasz Regiec <regiectomasz@gmail.com>
2019-03-17 13:19:25 +0100

    feat(UI): add basic UI router (#1)
    - install react-router-dom
    - add basic router
    - move csscomb config file to root project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
+} from 'react-router-dom';
 
 import './App.scss';
-import WeatherWidget from './components/weather-widget/weather-widget'
+import WeatherWidget from './components/weather-widget/weather-widget';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div id="app">
-                <WeatherWidget />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div id="app">
+				<Router>
+					<div>
+						<Switch>
+							<Route exact path="/" render={() => (
+								<Redirect to="/forecast5days"/>
+							)}/>
+							<Route path="/forecast5days" component={WeatherWidget}/>
+							<Route render={() => (
+								<div>
+									<h3>404 page not found</h3>
+								</div>
+							)}/>
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		);
+	}
 }
